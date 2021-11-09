18be7c4817cad17e0bd4416f75f32cd4386ad8c2
Ernesto Reig Alvarez <ernesto@Ernestos-MacBook-Pro.local>
2021-11-09 23:17:19 +0100

    hard one

--- src/main/java/org/erni/facebookprep/level1/UniformIntegers.java
+++ src/main/java/org/erni/facebookprep/level1/UniformIntegers.java
@@ -8,78 +8,83 @@ package org.erni.facebookprep.level1;
  */
 public class UniformIntegers {
 
-    public static int getLength (long A) {
-        int length = 0;
-        long temp = 1;
-        while (temp <= A) {
-            length++;
-            temp *= 10;
-        }
-        return length;
-    }
-
-    public static boolean isEqualOrGreaterThanUniform (String bString, int firstNumber) {
-        for (int i = 1; i < bString.length(); i++) {
-            if (Character.getNumericValue(bString.charAt(i)) < firstNumber) return false;
-        }
-
-        return true;
-    }
-
-    public static boolean isEqualOrLowerThanUniform (String bString, int firstNumber) {
-        for (int i = 1; i < bString.length(); i++) {
-            if (Character.getNumericValue(bString.charAt(i)) > firstNumber) return false;
-        }
-
-        return true;
-    }
-
-    public static boolean isUniform(String bString) {
-        char first = bString.charAt(0);
-        for (int i = 1; i < bString.length(); i++) {
-            if (bString.charAt(i) != first) return false;
-        }
-
-        return true;
-    }
-
     public static int getUniformIntegerCountInInterval(long A, long B) {
-        int uniformIntegers = 0;
-        int lengthA = getLength(A);
-        int lengthB = getLength(B);
-        String bString = String.valueOf(B);
-        int firstNumberB = Character.getNumericValue(bString.charAt(0));
-        String aString = String.valueOf(A);
-        int firstNumberA = Character.getNumericValue(aString.charAt(0));
+        int uniformNumbers = 0;
+        long i = A;
+        int power = (int) Math.log10(A); // initialize depending on As cardinality
 
+        if (i < 10) {
+            if (B <= 9) return (int) (B - i + 1);
+            else uniformNumbers = (int) (9 - i + 1);
+            i = 11;
+            power = 1;
+        }
+
+        // initialize current increment
+        long currentIncrement = 11;
+        for(int j = 1; j < power; j++) {
+            currentIncrement = currentIncrement * 10 + 1;
+        }
+
+        // initialize i if no divisible by currentIncrement
+        long rest = i % currentIncrement;
+        if(rest != 0) {
+            i = i + (currentIncrement - rest);
+        }
+
+        // here we know that if A == B, A and B are greater than 9
         if (A == B) {
-            if(isUniform(aString)) return 1;
-            return 0;
+            // special case A = 10 = B
+            if (A == 10) return 0;
+            long restA = A % currentIncrement;
+            if(restA == 0) return 1;
+            else return 0;
         }
 
-        if(lengthA == lengthB) {
-            if (lengthA == 1) return (int) (B - A) + 1;
-            // else
-            double div = Math.pow(10, lengthA-1);
-            uniformIntegers = (int) ((B - A) / div);
-        } else { // lengthB > lengthA as A is always <= B
-            uniformIntegers += (int) (9 * (Math.pow(10,lengthA-1)) - A) / Math.pow(10,lengthA-1);
-            uniformIntegers++; // counting A as 9 or 99 or 999 or 9999...
-            uniformIntegers += (lengthB - lengthA - 1) * 10;
-            uniformIntegers += firstNumberB - 1;
-            if (lengthA > 1 ) if (isEqualOrLowerThanUniform(aString,firstNumberA)) uniformIntegers++;
+        while (i < B) {
+            uniformNumbers++;
+            if(i > Math.pow(10,power+1)) {
+                power++;
+                i++;
+                currentIncrement = currentIncrement * 10 + 1;
+                if (i == B) {
+                    uniformNumbers++;
+                    break;
+                }
+            }
+            i += currentIncrement;
         }
-        if (isEqualOrGreaterThanUniform(bString, firstNumberB)) uniformIntegers++;
 
-        return uniformIntegers;
+        return uniformNumbers;
     }
 
     public static void main(String[] args) {
+        System.out.println(getUniformIntegerCountInInterval(5, 8)); // 4
+        System.out.println(getUniformIntegerCountInInterval(8, 8)); // 1
+        System.out.println(getUniformIntegerCountInInterval(8, 10)); // 2
+        System.out.println(getUniformIntegerCountInInterval(8, 13)); // 3
+        System.out.println(getUniformIntegerCountInInterval(8, 130)); // 12
+        System.out.println(getUniformIntegerCountInInterval(75, 300)); // 5
+
+        System.out.println(getUniformIntegerCountInInterval(7, 7)); // 1
+        System.out.println(getUniformIntegerCountInInterval(9, 9)); // 1
+        System.out.println(getUniformIntegerCountInInterval(10, 10)); // 0
+        System.out.println(getUniformIntegerCountInInterval(11, 11)); // 1
+        System.out.println(getUniformIntegerCountInInterval(22, 22)); // 1
+        System.out.println(getUniformIntegerCountInInterval(222, 222)); // 1
+        System.out.println(getUniformIntegerCountInInterval(532, 532)); // 0
+        System.out.println(getUniformIntegerCountInInterval(13000, 13000)); // 0
+        System.out.println(getUniformIntegerCountInInterval(45, 45)); // 0
+
+
+
+        //System.out.println(getUniformIntegerCountInInterval(8, 13)); // 3
+        //System.out.println(getUniformIntegerCountInInterval(8, 13)); // 3
         //System.out.println(getUniformIntegerCountInInterval(50, 80)); // 3
         //System.out.println(getUniformIntegerCountInInterval(7, 30)); // 6
         //System.out.println(getUniformIntegerCountInInterval(75, 300)); // 5
         //System.out.println(getUniformIntegerCountInInterval(99, 99)); // 1
-        System.out.println(getUniformIntegerCountInInterval(96, 96)); // 0
+        //System.out.println(getUniformIntegerCountInInterval(96, 96)); // 0
     }
 
 }
