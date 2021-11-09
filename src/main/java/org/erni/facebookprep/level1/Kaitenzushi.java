ce55083fe7a551cc87cd54e1757446dd43196b08
Ernesto Reig Alvarez <ernesto@Ernestos-MacBook-Pro.local>
2021-11-09 14:39:29 +0100

    added different ways to solve it, being the 4th one the most efficient due to the use of a Set to check contains()

--- src/main/java/org/erni/facebookprep/level1/Kaitenzushi.java
+++ src/main/java/org/erni/facebookprep/level1/Kaitenzushi.java
@@ -1,7 +1,6 @@
 package org.erni.facebookprep.level1;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 /**
  * There are N dishes in a row on a kaiten belt, with the ith dish being of type D_i.
@@ -32,10 +31,95 @@ public class Kaitenzushi {
         return totalEatenDishes;
     }
 
+    public static boolean alreadyEaten(int dish, List<Integer> eatenDishes) {
+        for (int i = 0; i < eatenDishes.size(); i++) {
+            if (dish == eatenDishes.get(i)) return true;
+        }
+        return false;
+    }
+
+    public static int getMaximumEatenDishCount2(int N, int[] D, int K) {
+        List<Integer> eatenDishes = new ArrayList<Integer>(K);
+        eatenDishes.add(D[0]); // we eat the first dish :)
+        int totalEatenDishes = 1;
+
+        for (int i = 1; i < N; i ++) {
+            if(!alreadyEaten(D[i], eatenDishes)) { // not already eaten
+                eatenDishes.add(D[i]);
+                totalEatenDishes++;
+                if(eatenDishes.size() > K) eatenDishes.remove(0);
+            }
+        }
+
+        return totalEatenDishes;
+    }
+
+    public static boolean alreadyEaten(int dish, int[] eatenDishes) {
+        for (int i = 0; i < eatenDishes.length; i++) {
+            if (dish == eatenDishes[i]) return true;
+        }
+        return false;
+    }
+
+    public static int getMaximumEatenDishCount3(int N, int[] D, int K) {
+        int[] eatenDishes = new int[K];
+        int index = 0;
+        eatenDishes[index] = D[0]; // we eat the first dish :)
+        int totalEatenDishes = 1;
+
+        for (int i = 1; i < N; i ++) {
+            if(!alreadyEaten(D[i], eatenDishes)) { // not already eaten
+                index++;
+                if(index == K) index = 0;
+                eatenDishes[index] = D[i]; // we eat it
+                totalEatenDishes++;
+            }
+        }
+
+        return totalEatenDishes;
+    }
+
+    public static int getMaximumEatenDishCount4(int N, int[] D, int K) {
+        Queue<Integer> eatenDishes = new LinkedList<Integer>();
+        Set<Integer> eatenDishesSet = new HashSet<Integer>(K);
+
+        // we eat the first dish :)
+        eatenDishes.add(D[0]);
+        eatenDishesSet.add(D[0]);
+        int totalEatenDishes = 1;
+
+        for (int i = 1; i < N; i ++) {
+            if(!eatenDishesSet.contains(D[i])) { // not already eaten
+                eatenDishes.add(D[i]);
+                eatenDishesSet.add(D[i]);
+                totalEatenDishes++;
+                if(eatenDishes.size() > K) {
+                    int dish = eatenDishes.remove();
+                    eatenDishesSet.remove(dish);
+                }
+            }
+        }
+
+        return totalEatenDishes;
+    }
+
     public static void main(String[] args) {
+        /*
         System.out.println(getMaximumEatenDishCount(6, new int[]{1,2,3,3,2,1}, 1));
         System.out.println(getMaximumEatenDishCount(6, new int[]{1,2,3,3,2,1}, 2));
         System.out.println(getMaximumEatenDishCount(7, new int[]{1,2,1,2,1,2,1}, 2));
+
+        System.out.println(getMaximumEatenDishCount(6, new int[]{1,2,3,3,2,1}, 1));
+        System.out.println(getMaximumEatenDishCount(6, new int[]{1,2,3,3,2,1}, 2));
+        System.out.println(getMaximumEatenDishCount(7, new int[]{1,2,1,2,1,2,1}, 2));
+
+        System.out.println(getMaximumEatenDishCount3(6, new int[]{1,2,3,3,2,1}, 1));
+        System.out.println(getMaximumEatenDishCount3(6, new int[]{1,2,3,3,2,1}, 2));
+        System.out.println(getMaximumEatenDishCount3(7, new int[]{1,2,1,2,1,2,1}, 2));
+        */
+        System.out.println(getMaximumEatenDishCount4(6, new int[]{1,2,3,3,2,1}, 1));
+        System.out.println(getMaximumEatenDishCount4(6, new int[]{1,2,3,3,2,1}, 2));
+        System.out.println(getMaximumEatenDishCount4(7, new int[]{1,2,1,2,1,2,1}, 2));
     }
 
 }
