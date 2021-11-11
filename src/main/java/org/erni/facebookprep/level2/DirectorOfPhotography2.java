fd9eb0622b9d28ac7af7d0b2ec1549b9be9b60c0
Ernesto Reig Alvarez <ernesto@Ernestos-MacBook-Pro.local>
2021-11-11 14:43:01 +0100

    2 different implementations. Second one is efficient but it fails 4 tests DON´T KNOW WHYgit status

--- src/main/java/org/erni/facebookprep/level2/DirectorOfPhotography2.java
+++ src/main/java/org/erni/facebookprep/level2/DirectorOfPhotography2.java
@@ -28,14 +28,17 @@ public class DirectorOfPhotography2 {
                 int BsRight = 0;
 
                 // Count Ps and Bs in the allowed distance to the left
+                char c;
                 for (int j = i - X; j >= i - Y && j > -1; j--) {
-                    if(C.charAt(j) == 'P') PsLeft++;
-                    else if(C.charAt(j) == 'B') BsLeft++;
+                    c = C.charAt(j);
+                    if(c == 'P') PsLeft++;
+                    else if(c == 'B') BsLeft++;
                 }
                 // Count Ps and Bs in the allowed distance to the right
                 for(int j = i + X; j <= i + Y && j < N; j++) {
-                    if(C.charAt(j) == 'P') PsRight++;
-                    else if(C.charAt(j) == 'B') BsRight++;
+                    c = C.charAt(j);
+                    if(c == 'P') PsRight++;
+                    else if(c == 'B') BsRight++;
                 }
 
                 artisticPhotographs += (PsLeft * BsRight) + (PsRight * BsLeft);
@@ -45,10 +48,94 @@ public class DirectorOfPhotography2 {
         return artisticPhotographs;
     }
 
+    public static long getArtisticPhotographCount2(int N, String C, int X, int Y) {
+        int leftWindowPs = 0;
+        int leftWindowBs = 0;
+        int rightWindowPs = 0;
+        int rightWindowBs = 0;
+        int artisticPhotographs = 0;
+        int LWLI = 0; // Left Window Lower Index
+        int LWUI = 0; // Left Window Upper Index
+        int RWLI = X + X - 1; // Right Window Lower Index, Starts at twice min distance
+        int RWUI = RWLI + (Y - X); // Right Window Upper Index, Starts at Right Lower Index + (max - min) distance
+        if (RWUI >= N) RWUI = N - 1;  // in the special case it´s initialized greater than length of C
+        int WINDOW_LENGTH = Y - X + 1;
+
+        // initialize left window to the first char
+        char c = C.charAt(0);
+        if (c == 'P') leftWindowPs++;
+        else if (c == 'B') leftWindowBs++;
+        int leftWindowLength = 1;
+
+        // we initialize the right window to its total length
+        for (int i = RWLI; i <= RWUI; i++) { // we start at RWLI
+            c = C.charAt(i);
+            if (c == 'P') rightWindowPs++;
+            else if (c == 'B') rightWindowBs++;
+        }
+
+        for (int i = X; i < N-X; i++) {
+            // let window upkeep
+            if(i - X > LWUI) { // if we have to increase (or move up one space) the window
+                LWUI++;
+                int lwuiChar = C.charAt(LWUI);
+                if (lwuiChar == 'P') leftWindowPs++;
+                else if (lwuiChar == 'B') leftWindowBs++;
+                leftWindowLength++;
+                if(leftWindowLength > WINDOW_LENGTH) {
+                    // increase lower left index
+                    int lwliChar = C.charAt(LWLI);
+                    if (lwliChar == 'P') leftWindowPs--;
+                    else if (lwliChar == 'B') leftWindowBs--;
+                    LWLI++;
+                    leftWindowLength--;
+                }
+            }
+
+            // right window upkeep
+            if (RWLI == N) break;
+            c = C.charAt(RWLI);
+            if (c == 'P') rightWindowPs--;
+            else if (c == 'B') rightWindowBs--;
+            RWLI++;
+            if(RWUI < N - 1) { // check if RWUI is already at the end
+                RWUI++;
+                c = C.charAt(RWUI);
+                if (c == 'P') rightWindowPs++;
+                else if (c == 'B') rightWindowBs++;
+            }
+
+            // start checking
+            if (C.charAt(i) == 'A') {
+                artisticPhotographs += (leftWindowPs * rightWindowBs) + (rightWindowPs * leftWindowBs);
+            }
+        }
+
+        return artisticPhotographs;
+    }
+
     public static void main(String[] args) {
-        //System.out.println(getArtisticPhotographCount(5, "APABA", 1, 2)); // 1
-        //System.out.println(getArtisticPhotographCount(5, "APABA", 2, 3)); // 0
-        System.out.println(getArtisticPhotographCount(8, ".PBAAP.B", 1, 3)); // 3
+        /*
+        System.out.println(getArtisticPhotographCount2(5, "APABA", 1, 2)); // 1
+        System.out.println(getArtisticPhotographCount2(5, "APABA", 2, 3)); // 0
+
+         */
+        System.out.println(getArtisticPhotographCount2(8, ".PBAAP.B", 1, 3)); // 3
+        /*
+        System.out.println(getArtisticPhotographCount2(8, ".PBAAP.B", 2, 3)); // 1
+        System.out.println(getArtisticPhotographCount2(12, ".PBAAAAAAP.B", 2, 3)); // 0
+        System.out.println(getArtisticPhotographCount2(3, "PBA", 1, 3)); // 0
+        System.out.println(getArtisticPhotographCount2(3, "PAB", 1, 3)); // 1
+        System.out.println(getArtisticPhotographCount2(3, "BAP", 1, 3)); // 1
+        System.out.println(getArtisticPhotographCount2(5, "BAPAB", 1, 3)); // 2
+        System.out.println(getArtisticPhotographCount2(5, "BAPAB", 1, 5)); // 2
+        System.out.println(getArtisticPhotographCount2(5, "BAPAA", 1, 5)); // 1
+        System.out.println(getArtisticPhotographCount2(6, "BAPABA", 1, 6)); // 2
+        System.out.println(getArtisticPhotographCount2(6, "BAPABA", 1, 1)); // 2
+
+         */
+
+        //System.out.println(getArtisticPhotographCount2(7, "PAA.BAB", 2, 4));
     }
 
 }
